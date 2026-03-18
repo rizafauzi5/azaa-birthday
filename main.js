4b1b305080285ab2be568e6a9e4e221e9dae61ee
rizafauzi5 <achmadriza004@gmail.com>
2026-03-19 01:12:09 +0700

    first

---
+++ main.js
@@ -0,0 +1,58 @@
+// --- Confetti Animation ---
+const canvas = document.getElementById('confetti');
+const ctx = canvas.getContext('2d');
+let particles = [];
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener('resize', resize);
+resize();
+
+class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height - canvas.height;
+        this.size = Math.random() * 7 + 3;
+        this.speed = Math.random() * 3 + 1;
+        this.color = `hsl(${Math.random() * 360}, 70%, 80%)`;
+        this.angle = Math.random() * 6.28;
+    }
+    update() {
+        this.y += this.speed;
+        this.x += Math.sin(this.angle) * 0.5;
+        if (this.y > canvas.height) {
+            this.y = -10;
+            this.x = Math.random() * canvas.width;
+        }
+    }
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+// Create particles
+for (let i = 0; i < 100; i++) {
+    particles.push(new Particle());
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach(p => {
+        p.update();
+        p.draw();
+    });
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+// --- Gift Interaction ---
+function showGift(message) {
+    alert(message);
+}
